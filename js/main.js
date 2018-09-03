3d90dca601edd39771245c111f0edae79c539b0c
phoebeyuer <yinyuefengshijie@qq.com>
2018-09-03 12:38:50 +0800

    lalal

--- js/main.js
+++ js/main.js
@@ -185,12 +185,16 @@ main();
 
 $(window).ready(function(){
 	console.log('ready正在加载');
-})
-
-$(window).on("load",function(){
-	console.log("所有页面已加载完");
 	$('#loadAction').css('display','none');
 	$('.right-nav').css('opacity','1');
 	$('.head-container').css('opacity','1');
 	$('.navbar .container').css('opacity','1');
+})
+
+$(window).on("load",function(){
+	console.log("所有页面已加载完");
+	// $('#loadAction').css('display','none');
+	// $('.right-nav').css('opacity','1');
+	// $('.head-container').css('opacity','1');
+	// $('.navbar .container').css('opacity','1');
 });
